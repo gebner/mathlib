e83dcd5e8f5c59e2876901f72aed2658d8c0bcef
Gabriel Ebner <gebner@gebner.org>
2020-01-06 05:18:13 +0100

    wip

--- src/tactic/hammer/mex1.lean
+++ src/tactic/hammer/mex1.lean
@@ -1,4 +1,4 @@
-import tactic.hammer.monomorphization2 data.nat.gcd data.real.basic
+import tactic.hammer data.nat.gcd data.real.basic
 
 example (x y : ℝ) (h : x < y) : ∃ z : ℚ, x < z ∧ (z : ℝ) < y :=
 by hammer3 20
--- src/tactic/hammer/mex2.lean
+++ src/tactic/hammer/mex2.lean
@@ -1,5 +1,5 @@
-import tactic.hammer.monomorphization data.nat.gcd data.real.basic
+import tactic.hammer data.nat.gcd data.real.basic
 
 example (x y : ℝ) (h : x < y) : ∃ z : ℚ, x < z ∧ (z : ℝ) ≤ y :=
--- by hammer2 60 -- bug due to uninhabited (fintype ℚ)
-by hammer2 [exists_rat_btwn, le_of_lt]
+by hammer3 60 -- bug due to uninhabited (fintype ℚ)
+-- by hammer2 [exists_rat_btwn, le_of_lt]
